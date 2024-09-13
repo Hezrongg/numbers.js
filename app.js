dfb42f63ab6294453fd6d7b768b9a016b07646e8
Hezrongg <hezrongg26@gmail.com>
2024-09-13 15:04:44 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const favNumber1 = 26;
+const favNumber2 = 6;
+const sum = favNumber1 + favNumber2;
+console.log(sum);
+
+const difference = favNumber1 - favNumber2;
+console.log(sum);
+
+const product = favNumber1 * favNumber2;
+console.log(product);
+
+const quotient = favNumber1 / favNumber2;
+console.log(quotient);
+
+const remainder = favNumber1 % favNumber2;
+console.log(remainder);
+
+const exponentiation = favNumber1 ** favNumber2;
+console.log(exponentiation);
